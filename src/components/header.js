7c43274aeb3beb5851bf976d7683987b87bc66e1
ryanbrown358 <ryanbrown358@gmail.com>
2017-12-30 22:36:26 -0700

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Header extends React.Component{
+
+    render(){
+        return(
+            <h1> React ToDo's</h1>
+        );
+
+        
+    }
+}
